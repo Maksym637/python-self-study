1e3edc7d3400db35d67971f892822f62cfd45ac2
Maksik <maksym.oliinyk.knm.2020@lpnu.ua>
2022-03-09 23:03:17 +0200

    new commit self-study.py

--- self-study.py
+++ self-study.py
@@ -1,1 +1,8 @@
-print("Hello, World!")
+
+welcome_guide_to_python = '''
+Hi!
+In this GitHub link,
+you will learn start informmation about python.
+Wish you a good luck in learning this interesting programmig language !'''
+
+print(welcome_guide_to_python)
